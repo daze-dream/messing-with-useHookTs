730396824ef45b75ccf448ad75abd1b46c304574
CheetoDustNeo <misterrolypoly@gmail.com>
2023-07-28 00:45:34 -0500

    fun tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CourseSequence, CourseTable } from './CourseTable'
-
-
+import "./App.css";
+import { CourseSequence, CourseTable } from "./CourseTable";
 
 function App() {
-  const [count, setCount] = useState(0);
-  
   const mockData: CourseSequence[] = [
     {
       sequence: 1,
-      courseId: '1',
-      credits: '1',
-      sharedPathways: 1
+      courseId: "Synthetic Goblins",
+      credits: "1",
+      sharedPathways: 1,
     },
     {
       sequence: 2,
-      courseId: '2',
-      credits: '2',
-      sharedPathways: 1
+      courseId: "How to Burn Villages 101",
+      credits: "2",
+      sharedPathways: 1,
     },
     {
       sequence: 2,
-      courseId: '3',
-      credits: '1',
-      sharedPathways: 1
-    },
-    {
-      sequence: 2,
-      courseId: '6',
-      credits: '1',
-      sharedPathways: 1
+      courseId: "Raiding and Looting 101",
+      credits: "1",
+      sharedPathways: 1,
     },
     {
       sequence: 3,
-      courseId: '4',
-      credits: '1',
-      sharedPathways: 1
+      courseId: "Standardized Equipment Studies",
+      credits: "1",
+      sharedPathways: 1,
     },
-  ]
+    {
+      sequence: 4,
+      courseId: "Rudimentary Mathematics",
+      credits: "1",
+      sharedPathways: 1,
+    },
+  ];
 
   return (
-    <>    <div>
-      <CourseTable courseData={mockData}/>
-    </div></>
-
-  )
-  
+    <>
+      <div>
+        <CourseTable courseData={mockData} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
