45daec0cbf5c0088360d544b9ae20ab2ebff1a7e
CheetoDustNeo <misterrolypoly@gmail.com>
2023-07-28 01:55:32 -0500

    more fun

--- src/CourseTable.tsx
+++ src/CourseTable.tsx
@@ -14,14 +14,14 @@ export type CourseTableProps = {
 
 export function CourseTable(props: CourseTableProps) {
     const { courseData } = props
-    const sequenceAndSize = new Map<number, number>();
+    const sequenceAndSpanSize = new Map<number, number>();
     courseData.forEach((course) => {
-        const found = sequenceAndSize.get(course.sequence);
+        const found = sequenceAndSpanSize.get(course.sequence);
         if (found !== undefined) {
-            sequenceAndSize.set(course.sequence, found + 1);
+            sequenceAndSpanSize.set(course.sequence, found + 1);
         }
         else {
-            sequenceAndSize.set(course.sequence, 1);
+            sequenceAndSpanSize.set(course.sequence, 1);
 
         }
     })
@@ -30,14 +30,13 @@ export function CourseTable(props: CourseTableProps) {
 
     courseData.forEach((course) => {
         let hasSequenceBeenMade: boolean = false
-        const courseRowSpan = sequenceAndSize.get(course.sequence);
+        const courseRowSpan = sequenceAndSpanSize.get(course.sequence);
         if (courseRowSpan !== undefined) {
             if (courseRowSpan > 1) {
                 hasSequenceBeenMade = multiCourseSequencesAdded.has(course.sequence);
                 if(!hasSequenceBeenMade) {
                     multiCourseSequencesAdded.add(course.sequence);
                 }
-                console.log(`has ${course.sequence} been made: `, hasSequenceBeenMade);
             }
             const elementToAdd = (<TableRow>
                 {!hasSequenceBeenMade && <TableCell rowSpan={courseRowSpan}>{course.sequence}</TableCell>}
@@ -48,7 +47,6 @@ export function CourseTable(props: CourseTableProps) {
             rowElements.push(elementToAdd)
         }
     })
-    console.log('HUZZAH');
     return (
         <TableContainer component={Paper}>
             <Table>
